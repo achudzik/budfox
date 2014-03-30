1c596923488ee07f36a61421b25d5d99779a1be0
Adam Chudzik <Adam.Chudzik@adm.uw.edu.pl>
2014-03-30 03:57:14 +0200

    Hello world added

---
+++ budfox-ws/src/main/java/me/chudzik/recruitment/vivus/web/HelloController.java
@@ -0,0 +1,14 @@
+package me.chudzik.recruitment.vivus.web;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Moto!";
+    }
+
+}

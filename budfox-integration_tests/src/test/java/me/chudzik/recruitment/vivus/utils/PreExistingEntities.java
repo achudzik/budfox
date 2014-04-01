7097c5a43d4a2b8b110994fb48fd2830592f2537
Adam Chudzik <Adam.Chudzik@adm.uw.edu.pl>
2014-04-02 01:10:16 +0200

    Small changes to PreExistingEntities

--- budfox-integration_tests/src/test/java/me/chudzik/recruitment/vivus/utils/PreExistingEntities.java
+++ budfox-integration_tests/src/test/java/me/chudzik/recruitment/vivus/utils/PreExistingEntities.java
@@ -7,14 +7,19 @@ import org.joda.money.Money;
 import org.joda.time.Period;
 
 import me.chudzik.recruitment.vivus.model.Client;
+import me.chudzik.recruitment.vivus.model.LoanApplication;
 
 public class PreExistingEntities {
 
+    public static final String VALID_PESEL = "68092005286";
+
     public static final Client VALID_CLIENT =
-            new Client.Builder().id(1l).identificationNumber("68092005286").build();
+            new Client.Builder().id(1l).identificationNumber(VALID_PESEL).build();
 
     public static final Period THREE_WEEKS_PERIOD = Period.weeks(3);
 
     public static final Money THREE_PLN = Money.of(CurrencyUnit.of("PLN"), new BigDecimal("3.0"));
 
+    public static final LoanApplication VALID_LOAN_APPLICATION =
+            new LoanApplication.Builder().client(VALID_CLIENT).amount(THREE_PLN).term(THREE_WEEKS_PERIOD).build();
 }

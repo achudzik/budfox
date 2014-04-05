93a78f1a9959185df2d7bbcb3e8f9b9274aa11f2
Adam Chudzik <Adam.Chudzik@adm.uw.edu.pl>
2014-04-05 22:02:47 +0200

    Jackson's ObjectMapper configurated to de/serialize Joda types in desired way

---
+++ budfox-ws/src/main/java/me/chudzik/recruitment/vivus/configuration/JsonMapperConfiguration.java
@@ -0,0 +1,33 @@
+package me.chudzik.recruitment.vivus.configuration;
+
+import me.chudzik.recruitment.vivus.support.json.MoneyModule;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+
+@Configuration
+public class JsonMapperConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return buildObjectMapper();
+    }
+
+    public static ObjectMapper buildObjectMapper() {
+        return new ObjectMapper()
+                .registerModules(
+                        new MoneyModule(),
+                        new JodaModule())
+                .disable(
+                        SerializationFeature.FAIL_ON_EMPTY_BEANS,
+                        SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .disable(
+                        DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
+                );
+    }
+}

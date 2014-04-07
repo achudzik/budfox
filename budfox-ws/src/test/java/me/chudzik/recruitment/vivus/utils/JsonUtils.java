994b7513151643d8b62c964a7e4f0537da4f0462
Adam Chudzik <Adam.Chudzik@adm.uw.edu.pl>
2014-04-07 02:23:14 +0200

    JsonUtils is using now a properly configured ObjectMapper

--- budfox-ws/src/test/java/me/chudzik/recruitment/vivus/utils/JsonUtils.java
+++ budfox-ws/src/test/java/me/chudzik/recruitment/vivus/utils/JsonUtils.java
@@ -2,23 +2,20 @@ package me.chudzik.recruitment.vivus.utils;
 
 import java.io.IOException;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import me.chudzik.recruitment.vivus.configuration.JsonMapperConfiguration;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonUtils {
 
+    private static final ObjectMapper mapper = JsonMapperConfiguration.buildObjectMapper();
+
     public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
-        return mapper().writeValueAsBytes(object);
+        return mapper.writeValueAsBytes(object);
     }
 
     public static String convertObjectToJsonString(Object object) throws IOException {
-        return mapper().writeValueAsString(object);
-    }
-
-    private static ObjectMapper mapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        return mapper;
+        return mapper.writeValueAsString(object);
     }
 
 }

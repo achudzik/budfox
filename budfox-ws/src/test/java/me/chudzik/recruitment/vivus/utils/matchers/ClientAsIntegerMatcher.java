3172ee0c148858b7f0355546b89db1c9b5d87380
Adam Chudzik <Adam.Chudzik@adm.uw.edu.pl>
2014-04-08 23:12:16 +0200

    ClientAsIntegerMatcher simplified

--- budfox-ws/src/test/java/me/chudzik/recruitment/vivus/utils/matchers/ClientAsIntegerMatcher.java
+++ budfox-ws/src/test/java/me/chudzik/recruitment/vivus/utils/matchers/ClientAsIntegerMatcher.java
@@ -2,26 +2,10 @@ package me.chudzik.recruitment.vivus.utils.matchers;
 
 import me.chudzik.recruitment.vivus.model.Client;
 
-import org.hamcrest.Description;
-import org.hamcrest.TypeSafeMatcher;
-
-public class ClientAsIntegerMatcher extends TypeSafeMatcher<Integer> {
-
-    private final Client wanted;
+public class ClientAsIntegerMatcher extends LongAsIntegerMatcher {
 
     public ClientAsIntegerMatcher(Client wanted) {
-        this.wanted = wanted;
-    }
-
-    @Override
-    public void describeTo(Description description) {
-        description.appendValue(wanted.getId());
-    }
-
-    @Override
-    protected boolean matchesSafely(Integer item) {
-        Long actualValue = Long.valueOf(item.longValue());
-        return actualValue.equals(wanted.getId());
+        super(wanted.getId());
     }
 
 }

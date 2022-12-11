6a68c6a128d864945ae83f173f749f0939df63ad
Adam Chudzik <adam@chudzik.io>
2022-12-11 12:45:22 +0100

    [tests] AdjustableTimeProviderSingleton.java adjusted to javax validation 2 - compiles; WIP

--- src/test-integration/java/io/chudzik/recruitment/budfox/utils/AdjustableTimeProviderSingleton.java
+++ src/test-integration/java/io/chudzik/recruitment/budfox/utils/AdjustableTimeProviderSingleton.java
@@ -1,7 +1,14 @@
 package io.chudzik.recruitment.budfox.utils;
 
-import org.hibernate.validator.spi.time.TimeProvider;
 import org.joda.time.DateTime;
+import org.joda.time.base.BaseDateTime;
+
+import javax.annotation.Nullable;
+import javax.validation.ClockProvider;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Optional;
 
 /**
  * Workaround for failing tests, helping the project to pass the whole build without major refactoring.
@@ -9,7 +16,7 @@ import org.joda.time.DateTime;
  * through `META-INF/validation.xml` property).
  */
 // FIXME-ach: use ThreadLocal instead of final static
-public class AdjustableTimeProviderSingleton implements TimeProvider {
+public class AdjustableTimeProviderSingleton implements ClockProvider {
 
     public final static AdjustableTimeProvider SINGLE_INSTANCE;
 
@@ -18,31 +25,39 @@ public class AdjustableTimeProviderSingleton implements TimeProvider {
     }
 
 
-    @Override
-    public long getCurrentTime() {
-        return SINGLE_INSTANCE.getCurrentTime();
-    }
-
-
-    public static long setTo(DateTime dateTimeToBeReturned) {
+    public static AdjustableTimeProvider setTo(@Nullable DateTime dateTimeToBeReturned) {
         return SINGLE_INSTANCE.setTo(dateTimeToBeReturned);
     }
 
+    @Override
+    public Clock getClock() {
+        return SINGLE_INSTANCE.getClock();
+    }
 
-    private static class AdjustableTimeProvider implements TimeProvider {
 
-        private DateTime fixedDateTime = DateTime.now();
+    private static class AdjustableTimeProvider implements ClockProvider {
+
+        private Clock fixedClock = toClock(DateTime.now());
 
 
         @Override
-        public long getCurrentTime() {
-            return fixedDateTime.getMillis();
+        public Clock getClock() {
+            return fixedClock;
         }
 
 
-        long setTo(DateTime dateTimeToBeReturned) {
-            this.fixedDateTime = dateTimeToBeReturned;
-            return getCurrentTime();
+        AdjustableTimeProvider setTo(@Nullable DateTime dateTimeToBeReturned) {
+            this.fixedClock = toClock(dateTimeToBeReturned);
+            return this;
+        }
+
+
+        private static Clock toClock(DateTime dateTime) {
+            return Optional.ofNullable(dateTime)
+                .map(BaseDateTime::getMillis)
+                .map(Instant::ofEpochMilli)
+                .map(fixedInstant -> Clock.fixed(fixedInstant, ZoneId.systemDefault()))
+                .orElse(Clock.system(ZoneId.systemDefault()));
         }
 
     }
